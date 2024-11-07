e81251d18fb11a3c7504a205b31f3c0f760c5237
Carlo Elvisi <carloelvisi01@outlook.it>
2024-11-07 15:29:49 +0100

    card si generano correttamente

--- js/main.js
+++ js/main.js
@@ -13,37 +13,25 @@
 const postRowOne = document.getElementById("row");
 const postRowTwo = document.getElementById("row-2");
 
-fetch("https://jsonplaceholder.typicode.com/photos?_limit=3")
+const postRow = document.getElementById("row");
+
+fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
   .then((resp) => resp.json())
   .then((posts) => {
     console.log(posts);
 
+    
     posts.forEach((post) => {
-      postRowOne.innerHTML += `<div class="col-md-3 mb-4">
-        <div class="card" style="width: 90%">
-        <img src = ${post.url} class="card-img-top" alt="..." />
-        <div class="card-body">
-        <p class="card-text">
-        ${post.title}
-        </p>
-        </div>
-        </div> `;
-    });
-  });
-fetch("https://jsonplaceholder.typicode.com/photos?_limit=3")
-  .then((resp) => resp.json())
-  .then((posts) => {
-    console.log(posts);
-
-    posts.forEach((post) => {
-      postRowTwo.innerHTML += `<div class="col-md-3 mb-4">
-        <div class="card" style="width: 90%">
-        <img src=  ${post.url} class="card-img-top" alt="..." />
-        <div class="card-body">
-        <p class="card-text">
-         ${post.title}
-        </p>
-        </div>
-        </div> `;
+      postRow.innerHTML += `
+        <div class="col-md-4 mb-4">
+          <div class="card" style="width: 90%">
+            <img src="${post.url}" class="card-img-top" alt="${post.title}" />
+            <div class="card-body">
+              <p class="card-text">
+                ${post.title}
+              </p>
+            </div>
+          </div>
+        </div>`;
     });
   });
